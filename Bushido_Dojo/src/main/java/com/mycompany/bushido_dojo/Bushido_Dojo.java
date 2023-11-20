ffce3a04cd89147bf3b1ccb064da474dfd33467f
Seu Nome <joaocoromberk@gmail.com>
2023-11-20 18:04:55 -0300

    commit forms

---
+++ Bushido_Dojo/src/main/java/com/mycompany/bushido_dojo/Bushido_Dojo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bushido_dojo;
+
+/**
+ *
+ * @author dudac
+ */
+public class Bushido_Dojo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

624d591eec14a66a51f663c3d223fde6e6a83b54
Seu Nome <joaocoromberk@gmail.com>
2023-11-20 21:24:47 -0300

    commit odicial

---
+++ Bushido_Dojo/src/main/java/com/mycompany/bushido_dojo/Bushido_Dojo.java
@@ -0,0 +1,10 @@
+package com.mycompany.bushido_dojo;
+
+public class Bushido_Dojo extends jframeBushido_Dojo{
+
+    public static void main(String[] args) {
+        jframeBushido_Dojo login = new jframeBushido_Dojo();
+        login.setVisible(true);
+        
+    }
+}
